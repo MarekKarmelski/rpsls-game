0b2487b06f77dba3b96ebf31fca6e9b34700163b
Marek Karmelski <marek.karmelski@gmail.com>
2017-03-19 16:59:59 +0100

    Modify classes

--- src/lizard.py
+++ src/lizard.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from rpsls.src.element import Element
+from src.element import Element
 
 """Lizard class."""
 
--- src/rock.py
+++ src/rock.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from src
+from src.element import Element
 
 """Rock class."""
 
